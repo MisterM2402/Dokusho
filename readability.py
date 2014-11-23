0db210d85673174eb65728beac9ba8837ad47083
Michael Mair <s1016269@sms.ed.ac.uk>
2014-11-23 02:04:43 +0000

    Added Flesch-Kincaid and Dale-Chall measures, plus ReadabilityText class. Dale-Chall is quite slow and unsure whether it gives the right number - probably needs fixed (or a different measure chosen).

--- readability.py
+++ readability.py
@@ -2,42 +2,59 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import gutenberg
 
 
-def avg_sent_len(text):
-    sents = sent_tokenize(text)
+class ReadabilityText():
+
+    def __init__(self, text):
+        self.words = word_tokenize(text)
+        self.sents = sent_tokenize(text)
+        self.len = len(self.words)
+        self.avg_sent_len = calc_avg_sent_len(self.sents)
+        self.avg_syl = calc_avg_syl(self.words)
+
+
+def flesch_kincaid(r_text):
+    return 0.39 * r_text.avg_sent_len + 11.8 * r_text.avg_syl - 15.59
+
+
+def dale_chall(r_text, easy_words):
+    words = r_text.words
+    dw_count = 0
+    for w in words:
+        if w.lower() not in easy_words:
+            dw_count += 1
+    dw_percentage = dw_count * 100.0 / r_text.len
+    adjust = 0
+    if dw_percentage > 5:
+        adjust = 3.6365
+    return 0.1579 * dw_percentage + 0.0496 * r_text.avg_sent_len + adjust
+
+
+def calc_avg_sent_len(sents):
     sent_lens = []
     for s in sents:
         sent_lens.append(len(word_tokenize(s)))
     return sum(sent_lens) / len(sents)
 
 
-def alt_avg_sent_len(text):
-    sents = text.split('.')
-    sent_lens = []
-    for s in sents:
-        sent_lens.append(len(s.split()))
-    return sum(sent_lens) / len(sents)
-
-
-def syl_count(text):
+def syl_count(words):
     count = 0
     vowels = 'aeiouy'
-    words = text.split()
     for w in words:
+        w = w.lower()
         prev_l = 'x'
         w_count = 0
         for l in w:
             if prev_l not in vowels and l in vowels:
                 w_count += 1
-                prev_l = l
-        #if len(w) > 2 and w_count > 0 and w[-2] not in vowels and w[-1] == 'e':
-        #    w_count -= 1
+            prev_l = l
+        if len(w) > 2 and w[-2] not in vowels and w[-1] == 'e':
+            w_count -= 1
         count += w_count
     return count
 
 
-def avg_syl(text):
-    words = text.split()
-    return 1.0 * syl_count(text) / len(words)
+def calc_avg_syl(words):
+    return 1.0 * syl_count(words) / len(words)
 
 
 if __name__ == '__main__':
@@ -75,9 +92,15 @@ dipped suddenly down, so suddenly that Alice had not a moment to think
 about stopping herself before she found herself falling down a very deep
 well."""
 
-    print 'Alice extract', syl_count(text1), avg_syl(text1)
+    with open('DaleChallEasyWordList.txt', 'r') as dcewl_file:
+        easy_words = [w.strip('\n') for w in dcewl_file.readlines()]
 
-    #for fid in gutenberg.fileids():
-    #    text = gutenberg.raw(fid)
-    #    print fid, len(text.split()), syl_count(text), avg_syl(text)
-        #print fid, avg_sent_len(text), alt_avg_sent_len(text)
+    r_text1 = ReadabilityText(text1)
+    print 'Alice extract', syl_count(r_text1.words), calc_avg_syl(r_text1.words), dale_chall(r_text1, easy_words)
+
+    for fid in gutenberg.fileids():
+        r_text = ReadabilityText(gutenberg.raw(fid))
+        if fid == 'carroll-alice.txt':
+            print fid, flesch_kincaid(r_text), dale_chall(r_text, easy_words)
+        #print fid, len(text.split()), syl_count(text), calc_avg_syl(text)
+        #print fid, calc_avg_sent_len(text), alt_avg_sent_len(text)
